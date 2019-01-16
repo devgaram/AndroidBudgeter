91e4bd8bbf8f15dd3d3a7bb41cb1c187f6c52f86
devgaram <joingaram@gmail.com>
2019-01-16 23:51:33 +0900

    감정 입모양 그리기 & 애니메이션

--- app/src/main/java/org/androidtown/mybudgeter/MainActivity.java
+++ app/src/main/java/org/androidtown/mybudgeter/MainActivity.java
@@ -1,10 +1,15 @@
 package org.androidtown.mybudgeter;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable emotionAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         BudgetFragment budgetFragment = (BudgetFragment) fragmentManager.findFragmentById(R.id.budget_fragment);
         //SpendListFragment spendListFragment = (SpendListFragment) fragmentManager.findFragmentById(R.id.spend_list_fragment);
+        ImageView emotionImage = (ImageView) findViewById(R.id.emotion_mouse);
+        emotionAnimation = (AnimationDrawable) emotionImage.getDrawable();
+
+        emotionImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emotionAnimation.start();
+            }
+        });
     }
 }
