11b74acfd0c49b1282ea96cc875459e7a266b7b5
devgaram <joingaram@gmail.com>
2019-01-17 00:19:39 +0900

    입모양 애니메이션 페이드인 아웃 적용

--- app/src/main/java/org/androidtown/mybudgeter/MainActivity.java
+++ app/src/main/java/org/androidtown/mybudgeter/MainActivity.java
@@ -4,12 +4,12 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     AnimationDrawable emotionAnimation;
+    ImageView emotionImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +19,30 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         BudgetFragment budgetFragment = (BudgetFragment) fragmentManager.findFragmentById(R.id.budget_fragment);
         //SpendListFragment spendListFragment = (SpendListFragment) fragmentManager.findFragmentById(R.id.spend_list_fragment);
-        ImageView emotionImage = (ImageView) findViewById(R.id.emotion_mouse);
+        emotionImage = (ImageView) findViewById(R.id.emotion_mouse);
         emotionAnimation = (AnimationDrawable) emotionImage.getDrawable();
-
-        emotionImage.setOnClickListener(new View.OnClickListener() {
+        emotionAnimation.setEnterFadeDuration(1000);
+        emotionAnimation.setExitFadeDuration(1000);
+        emotionAnimation.start();/*
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(Animation.RESTART);
+        emotionImage.setAnimation(animation);
+        animation.start();
+        animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onClick(View view) {
-                emotionAnimation.start();
+            public void onAnimationStart(Animation animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation fadeOut = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
+                emotionImage.startAnimation(fadeOut);
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
             }
         });
+*/
+
     }
 }
