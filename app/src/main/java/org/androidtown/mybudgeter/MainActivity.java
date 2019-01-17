615d9bcc65be3da52894f8e24e4c8d0528bf151e
devgaram <joingaram@gmail.com>
2019-01-18 01:32:43 +0900

    입 애니메이션 vector로 변경

--- app/src/main/java/org/androidtown/mybudgeter/MainActivity.java
+++ app/src/main/java/org/androidtown/mybudgeter/MainActivity.java
@@ -1,7 +1,11 @@
 package org.androidtown.mybudgeter;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -18,12 +22,30 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         BudgetFragment budgetFragment = (BudgetFragment) fragmentManager.findFragmentById(R.id.budget_fragment);
+
+
+            ImageView imageView = (ImageView) findViewById(R.id.emotion_mouse);
+            Drawable drawable = imageView.getDrawable();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                if (drawable instanceof AnimatedVectorDrawable) {
+                    AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) drawable;
+                    animatedVectorDrawable.start();
+                }
+            } else {
+                if (drawable instanceof AnimatedVectorDrawableCompat) {
+                    AnimatedVectorDrawableCompat animatedVectorDrawableCompat = (AnimatedVectorDrawableCompat) drawable;
+                    animatedVectorDrawableCompat.start();
+                }
+            }
+
         //SpendListFragment spendListFragment = (SpendListFragment) fragmentManager.findFragmentById(R.id.spend_list_fragment);
-        emotionImage = (ImageView) findViewById(R.id.emotion_mouse);
-        emotionAnimation = (AnimationDrawable) emotionImage.getDrawable();
-        emotionAnimation.setEnterFadeDuration(1000);
-        emotionAnimation.setExitFadeDuration(1000);
-        emotionAnimation.start();/*
+        //emotionImage = (ImageView) findViewById(R.id.emotion_mouse);
+        //emotionImage.setImageResource(R.drawable.ani_emotion_mouth);
+        //emotionAnimation = (AnimationDrawable) emotionImage.getDrawable();
+        //emotionAnimation.setEnterFadeDuration(1000);
+        //emotionAnimation.setExitFadeDuration(1000);
+        //emotionAnimation.start();
+        /*
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         animation.setRepeatCount(Animation.INFINITE);
         animation.setRepeatMode(Animation.RESTART);
