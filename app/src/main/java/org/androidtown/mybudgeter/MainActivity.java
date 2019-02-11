910b8e2129e6eacd5e55dd9326fa55f7413ecd95
devgaram <joingaram@gmail.com>
2019-02-11 23:37:59 +0900

    레이아웃 수정 및 뷰페이저 추가

--- app/src/main/java/org/androidtown/mybudgeter/MainActivity.java
+++ app/src/main/java/org/androidtown/mybudgeter/MainActivity.java
@@ -2,8 +2,11 @@ package org.androidtown.mybudgeter;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.design.bottomappbar.BottomAppBar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,11 +14,18 @@ public class MainActivity extends AppCompatActivity {
     AnimationDrawable emotionAnimation;
     ImageView emotionImage;
     Toolbar myToolbar;
+    BottomAppBar bottomAppBar;
+    Menu bottomAppBarMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(myPagerAdapter);
 
     }
+
+
 }
