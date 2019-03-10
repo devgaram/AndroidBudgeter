654ac4380c62377d376a89d755b8d9f1e7cca1fd
devgaram <joingaram@gmail.com>
2019-03-10 21:56:30 +0900

    오늘의 지출에 데이터 연결

---
+++ app/src/main/java/org/androidtown/mybudgeter/pager/ExpenditureRecyclerAdapter.java
@@ -0,0 +1,53 @@
+package org.androidtown.mybudgeter.pager;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.androidtown.mybudgeter.R;
+import org.androidtown.mybudgeter.expenditure.Expenditure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpenditureRecyclerAdapter extends RecyclerView.Adapter<ExpenditureRecyclerAdapter.ExpenditureHolder> {
+    private List<Expenditure> expenditures = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ExpenditureHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_expenditure, viewGroup, false);
+        return new ExpenditureHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExpenditureRecyclerAdapter.ExpenditureHolder expenditureHolder, int i) {
+        Expenditure currentExpenditure =expenditures.get(i);
+        expenditureHolder.expenditureMemo.setText(currentExpenditure.getMemo());
+        expenditureHolder.expenditureAmount.setText(Integer.toString(currentExpenditure.getAmount()));
+    }
+
+    public void setExpenditures(List<Expenditure> expenditures) {
+        this.expenditures = expenditures;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return expenditures.size();
+    }
+
+    public static class ExpenditureHolder extends RecyclerView.ViewHolder {
+        private TextView expenditureMemo;
+        private TextView expenditureAmount;
+
+        public ExpenditureHolder(View itemView) {
+            super(itemView);
+            expenditureMemo = (TextView) itemView.findViewById(R.id.item_expenditure_memo);
+            expenditureAmount = (TextView) itemView.findViewById(R.id.item_expenditure_amount);
+        }
+    }
+}
--- app/src/main/java/org/androidtown/mybudgeter/pager/PagerFragment.java
+++ app/src/main/java/org/androidtown/mybudgeter/pager/PagerFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +31,7 @@ public class PagerFragment extends Fragment {
     private int budgetId;
     private int totalExpenditureAmount;
     private int totalBudgetAmount;
+    private ExpenditureRecyclerAdapter expenditureRecyclerAdapter;
 
     public static PagerFragment newInstance() {
         return new PagerFragment();
@@ -48,6 +51,11 @@ public class PagerFragment extends Fragment {
             decimalFormat.applyPattern("#,#00");
             totalExpenditureAmount = 0;
             totalBudgetAmount = 0;
+
+            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.today_expenditure_list);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            expenditureRecyclerAdapter = new ExpenditureRecyclerAdapter();
+            recyclerView.setAdapter(expenditureRecyclerAdapter);
         }
         return view;
     }
@@ -74,6 +82,7 @@ public class PagerFragment extends Fragment {
             @Override
             public void onChanged(@Nullable List<Expenditure> expenditures) {
                 if (expenditures != null) {
+                    expenditureRecyclerAdapter.setExpenditures(expenditures);
                     for (Expenditure expenditure : expenditures) {
                         totalExpenditureAmount += expenditure.getAmount();
                     }
